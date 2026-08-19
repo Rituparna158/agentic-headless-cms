3528fbd958bf020abd0dbfe60010b654b43add65
sobhan-s <sobhans@mindfiresolutions.com>
2026-08-19 15:29:29 +0530

    feat(api): update middleware

--- packages/middlewares/src/types/express.d.ts
+++ packages/middlewares/src/types/express.d.ts
@@ -1,6 +1,6 @@
 import type { AuthenticatedUser } from '@repo/types';
 import type { SchemaRecord } from '@repo/shared-db';
-
+import type { RequestContext } from '@repo/context';
 declare global {
   namespace Express {
     export interface Request {
@@ -8,6 +8,7 @@ declare global {
       schema?: SchemaRecord;
       schemaId?: string;
       id?: string | number | object;
+      context?: RequestContext;
     }
   }
 }
